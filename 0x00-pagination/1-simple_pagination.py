dbae648e7abf12b06a4b9ad7b9e322239301fc0f
Ayokayzy <theayokayzy1@gmail.com>
2024-11-19 11:01:35 +0100

    Task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,7 +37,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
+        """
         Get Page Docs
         """
         assert isinstance(page, int) and page > 0
